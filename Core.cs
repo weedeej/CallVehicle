614be78792e89aabfcf8714c46753f62a704388b
weedeej <work.deeej@gmail.com>
2025-04-17 21:39:02 +0800

    Add project files.

---
+++ Core.cs
@@ -0,0 +1,18 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(CallVehicle.Core), "CallVehicle", "1.0.0", "Dixie", null)]
+[assembly: MelonGame("TVGS", "Schedule I")]
+
+namespace CallVehicle
+{
+    public class Core : MelonMod
+    {
+
+        public override void OnInitializeMelon()
+        {
+            // Initialize the mod
+            MelonLogger.Msg("CallVehicle mod initialized.");
+        }
+    }
+}
